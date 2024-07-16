9005aed88d346f5d5ac28e23184701a14eb00298
Roqeaiyakhan <roqaiyajaani@gmail.com>
2024-07-16 13:49:14 +0500

    js chp 21,22 class-ex

---
+++ index.js
@@ -0,0 +1,28 @@
+// var string = "Welcome to JavaScript!"
+// var string = string.length;
+// console.log(string);
+
+
+// var names = "Programming is awesome";
+// // var names = names.length;
+// // console.log(names)
+// var sliced = names.slice(15);
+// console.log(sliced);
+
+
+
+//programm:3
+// var str = "Hello, Universe!"
+// var sliced = str.slice(0,5)
+// console.log(sliced);
+
+//programm:4
+// var str = "Full Stack Developer";
+// var sliced = str.slice(5,10)
+// console.log(sliced)
+
+
+// //programm:5
+// var str = "Javascript Strings"
+// var sliced = str.slice(14)
+// console.log(sliced);
